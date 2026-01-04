746d3339761f46545e863adc4d429909a663d6b0
redbdb <gustavoasamaral@gmail.com>
2026-01-04 17:23:16 -0300

    tela de lembretes v1

--- aspa/lib/src/pages/reminders_page.dart
+++ aspa/lib/src/pages/reminders_page.dart
@@ -1,11 +1,105 @@
 import 'package:flutter/material.dart';
 
-class RemindersPage extends StatelessWidget {
+class RemindersPage extends StatefulWidget {
   const RemindersPage({super.key});
   @override
+  State<RemindersPage> createState() => _RemindersPageState();
+}
+
+class _RemindersPageState extends State<RemindersPage> {
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(title: const Text('Lembretes')),
-        body: const Center(child: Text('Tela de lembretes')));
+    return GestureDetector(
+      onTap: () => FocusScope.of(context).unfocus(),
+      child: Scaffold(
+        key: scaffoldKey,
+        body: SafeArea(
+          child: Column(
+            children: [
+              /// HEADER
+              Row(
+                children: [
+                  Padding(
+                    padding: const EdgeInsets.only(left: 29),
+                    child: IconButton(
+                      icon: const Icon(
+                        Icons.arrow_back_ios_new_rounded,
+                        size: 28,
+                      ),
+                      onPressed: () => Navigator.pop(context),
+                    ),
+                  ),
+                  const Expanded(
+                    child: Center(
+                      child: Padding(
+                        padding: EdgeInsets.only(right: 20),
+                        child: Text(
+                          'Lembretes',
+                          style: TextStyle(
+                            fontSize: 24,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.only(right: 20),
+                    child: Container(
+                      width: 29,
+                      height: 29,
+                      decoration: const BoxDecoration(
+                        shape: BoxShape.circle,
+                      ),
+                      child: const Icon(
+                        Icons.add,
+                        size: 24,
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+
+              /// MEDICAÇÕES
+              const Padding(
+                padding: EdgeInsets.only(top: 36),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 30),
+                    child: Text(
+                      'Medicações',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
+                  ),
+                ),
+              ),
+
+              const SizedBox(height: 28),
+
+              /// EXERCÍCIOS
+              const Align(
+                alignment: Alignment.centerLeft,
+                child: Padding(
+                  padding: EdgeInsets.only(left: 30),
+                  child: Text(
+                    'Exercícios',
+                    style: TextStyle(
+                      fontSize: 24,
+                      fontWeight: FontWeight.w500,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
