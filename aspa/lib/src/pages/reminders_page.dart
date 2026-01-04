423fe86fac172438499e73e5d619186e23e397f8
redbdb <gustavoasamaral@gmail.com>
2026-01-04 15:22:46 -0300

    arquivo da aba de lembretes adicionado ainda falta fazer ela

---
+++ aspa/lib/src/pages/reminders_page.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class RemindersPage extends StatelessWidget {
+  const RemindersPage({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Lembretes')),
+        body: const Center(child: Text('Tela de lembretes')));
+  }
+}
